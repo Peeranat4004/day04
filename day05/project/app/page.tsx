05060ba1932dcf0ac12b92600d1ba69bcf1216f3
Peeranat Chuenkeaw <s6530613011@phuket.psu.ac.th>
2025-04-03 10:25:48 +0700

    update page

--- day05/project/app/page.tsx
+++ day05/project/app/page.tsx
@@ -1,5 +1,9 @@
 export default function project() {
-  return (
-    <div> Hello world! I here Pee</div>
+  return ( <div><h1>Hello world! I here Pee 
+    </h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut hic facilis tempore id cupiditate illum!
+    </p>
+    </div>
   )
 }
