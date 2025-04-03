5693a97ebdf7263570c145ffdac86144f1e2a0cd
Peeranat Chuenkeaw <s6530613011@phuket.psu.ac.th>
2025-04-03 10:51:21 +0700

    update web

--- day05/project/app/page.tsx
+++ day05/project/app/page.tsx
@@ -1,7 +1,14 @@
 export default function project() {
-  return ( <div><h1>Hello world! I here Pee 
+  return ( <div className="w-[80%] border mb-2">
+    <h1 className="text-2xl font-bold m-2 p-2">Hello world! I here Pee 
     </h1>
-    <p>
+    <p className="w-64 border mb-4 ml-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut hic facilis tempore id cupiditate illum!
+    </p>
+    <p className="w-64 border mb-4 ml-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut hic facilis tempore id cupiditate illum!
+    </p>
+    <p className="w-64 border mb-4 ml-4">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut hic facilis tempore id cupiditate illum!
     </p>
     </div>
