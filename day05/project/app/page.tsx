15cc36ec7398f2cb39e2108b9a6f0fd6bc19435a
Peeranat Chuenkeaw <s6530613011@phuket.psu.ac.th>
2025-04-03 11:45:25 +0700

    update

--- day05/project/app/page.tsx
+++ day05/project/app/page.tsx
@@ -1,38 +1,52 @@
 import Image from 'next/image'
+import { Bungee_Tint } from "next/font/google";
+
+const bungee = Bungee_Tint({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export default function project() {
-  return ( <div className="w-[80%] border mb-2">
+  return (<div>
+    {/* grid-cols-1 gap-4 
+      sm:grid-cols-2
+      md:grid-cols-3
+      lg:grid-cols-4
+      xl:grid-cols-5
 
-    <h1 className="text-2xl font-bold m-2 p-2">Hello world! I here Pee 
-    </h1>
+      flex flexwrap */}
 
-    <Image
-      className='p-2 m-4'
-      src="https://computing.psu.ac.th/th/wp-content/uploads/2023/09/PSU-CoC-ENG_01_x49.png"
-      width={150}
-      height={150}
-      alt="PSU Logo"
-    />
+    <h1 className={`${bungee.className} "font-bold m-2 p-2"`}>Hello world!!</h1>
 
-    <Image
-      className='p-2 m-4'
-      src="/next.svg"
-      alt="Picture of the author"
-      width={150}
-      height={150} 
-      // alt="Next.js Logo"
-    />
+    <div className="w-[80%] border rounded p-4 m-auto bg-slate-100 
+    grid 
+    grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4">
+      <Image
+        className='p-2 m-4'
+        src="https://computing.psu.ac.th/th/wp-content/uploads/2023/09/PSU-CoC-ENG_01_x49.png"
+        width={150}
+        height={150}
+        alt="PSU Logo"
+      />
 
+      <Image
+        className="p-2 m-4"
+        src="/next.svg"
+        width={150}
+        height={150}
+        alt="Next.js Logo"
+      />
 
-    <p className="w-64 border mb-4 ml-4 bg-blue-200 text-black" >
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut hic facilis tempore id cupiditate illum!
-    </p>
-    <p className="w-64 border mb-4 ml-4 bg-blue-600 text-black">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut hic facilis tempore id cupiditate illum!
-    </p>
-    <p className="w-64 border mb-4 ml-4 bg-yellow-200 text-black ">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut hic facilis tempore id cupiditate illum!
-    </p>
+      <div className="w-64 p-2 border mb-2 rounded bg-blue-200 hover:bg-blue-700 hover:text-blue-50">
+        Lorem idivsum, dolor sit amet consectetur adidivisicing elit. Doloribus, consequuntur.
+      </div>
+      <div className="w-64 p-2 border mb-2 rounded bg-blue-600 hover:bg-blue-700 hover:text-blue-50">
+        Lorem idivsum, dolor sit amet consectetur adidivisicing elit. Doloribus, consequuntur.
+      </div>
+      <div className="w-64 p-2 border rounded bg-red-300 hover:bg-black-700 hover:text-blue-50">
+        Lorem idivsum, dolor sit amet consectetur adipisicing elit. Doloribus, consequuntur.
+      </div>
     </div>
-  )
+
+  </div>)
 }
